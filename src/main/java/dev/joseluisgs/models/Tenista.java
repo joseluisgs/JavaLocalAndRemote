9ab367ee7ffd7a720cc01355e402ad98952ae65d
José Luis González <joseluisgonsan@gmail.com>
2024-07-01 09:52:54 +0200

    Tenista Model

--- src/main/java/dev/joseluisgs/models/Tenista.java
+++ src/main/java/dev/joseluisgs/models/Tenista.java
@@ -1,4 +1,33 @@
 package dev.joseluisgs.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Tenista {
+    public static final long NEW_ID = 0L;
+
+    private long id = NEW_ID;
+    private String nombre;
+    private String pais;
+    private int altura;
+    private int peso;
+    private int puntos;
+    private Mano mano;
+    private LocalDate fechaNacimiento;
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime updatedAt = LocalDateTime.now();
+    private boolean isDeleted = false;
+
+    public enum Mano {
+        DIESTRO,
+        ZURDO
+    }
+    
 }
